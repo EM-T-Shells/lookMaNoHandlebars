3143547621761c885b59ce18dc011bda89146131
unknown <taskaleg@gmail.com>
2023-01-07 23:24:03 -0600

    updated

--- server.js
+++ server.js
@@ -8,38 +8,37 @@ const sequelize = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Create the Handlebars.js engine object with custom helper functions
-const hbs = exphbs.create({ helpers });
-// Inform Express.js which template engine we're using
-app.engine('handlebars', hbs.engine);
+const hbs = exphbs.create({ helpers }); // Create the Handlebars.js engine object with custom helper functions
+app.engine('handlebars', hbs.engine); // Inform Express.js which template engine we're using
 app.set('view engine', 'handlebars');
 
+const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
 const sess = {
   secret: 'Super secret secret',
-  cookie: {maxAge: 60 * 60 * 1000, httpOnly: true, secure: false},
-  resave: false,
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+  },
+  resave: true,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
   })
 };
-
+app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// 1/4 @ 22:45 - et //
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-
-
 app.use(routes);
+
+
+const PORT = process.env.PORT || 3001;
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
