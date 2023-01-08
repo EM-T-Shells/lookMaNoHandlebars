6f4ef5d3d500e405f060cb673c5a8a94852a2605
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2023-01-07 19:26:43 -0600

    add conditionals to get route

--- public/js/plant.js
+++ public/js/plant.js
@@ -23,7 +23,7 @@ const submitTaskHandler = async (event) => {
 
   if (watered.checked || pruned.checked || fertilized.checked || transplanted.checked || harvested.checked || applied.checked) {
     console.log("##################")
-    const response = await fetch(`/api/tasks/`, {
+    const response = await fetch('/api/tasks/', {
       method: 'POST',
       body: JSON.stringify(bodyObject),
       headers: { 'Content-Type': 'application/json' },
